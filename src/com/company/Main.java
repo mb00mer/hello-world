0d4a6dc36519c8cecf2f6a50f14520e4b38a5d74
Mikhail Baranchik <michael.baranchik@gmail.com>
2020-04-18 23:33:02 +0400

    init: hello-world application

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int a = 1;
+//        int b = 2;
+//        int c = a + b;
+//        System.out.println("a > b = " + (a > b));
+//        System.out.println("a < b = " + (a < b));
+
+        String hello = "Hello";
+        String world = "World";
+        System.out.println(hello + " " + world + "!");
+    }
+}
